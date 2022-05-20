ad399bf15763e70e7492684cc49accb612aed8da
Eliseu Videira <eliseuvideira1@gmail.com>
2022-05-20 17:11:38 -0300

    :sparkles: Initial setup done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import {
-  differenceInDays,
   differenceInHours,
   differenceInMinutes,
   differenceInSeconds,
@@ -7,6 +6,7 @@ import {
 import { useEffect, useState } from "react";
 import { getNextDayOfWeek } from "../functions/getNextDayOfWeek";
 import styles from "./index.module.scss";
+import { BsTwitter } from "react-icons/bs";
 
 // Date.now = (() => {
 //   const now = new Date();
@@ -24,15 +24,17 @@ const Home = () => {
   const [seconds, setSeconds] = useState(getDifference());
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      const difference = getDifference();
+    if (typeof window !== undefined) {
+      const interval = setInterval(() => {
+        const difference = getDifference();
 
-      if (difference != seconds) {
-        setSeconds(difference);
-      }
-    }, 100);
+        if (difference != seconds) {
+          setSeconds(difference);
+        }
+      }, 100);
 
-    return () => clearInterval(interval);
+      return () => clearInterval(interval);
+    }
   }, []);
 
   const now = Date.now();
@@ -43,11 +45,17 @@ const Home = () => {
     seconds: differenceInSeconds(now + seconds * 1000, now) % 60,
   };
 
+  const time = `${difference.hours
+    .toString()
+    .padStart(2, "0")}:${difference.minutes
+    .toString()
+    .padStart(2, "0")}:${difference.seconds.toString().padStart(2, "0")}`;
+
   return (
     <div className={styles.root}>
       <header>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary border-bottom border-secondary">
-          <div className="container-fluid">
+          <div className="container-fluid px-5">
             <a
               className={`${styles.brand} navbar-brand fs-1 fw-bolder`}
               href="#"
@@ -58,25 +66,31 @@ const Home = () => {
         </nav>
       </header>
       <main className="bg-primary text-white">
-        <div className="container-fluid h-100">
+        <div className={`${styles.main} container-fluid d-grid pb-5`}>
           <div className="row">
             <div className="col">
-              <div className="pt-5">
+              <div className="px-5 pt-5">
                 <p className="fw-bold fs-2">Get ready for #TezosTeusday!</p>
               </div>
             </div>
           </div>
-          <div className="row h-100">
-            <div className="col h-100">
-              <div className="h-75 d-flex align-items-center justify-content-center">
-                <div className={`${styles.timer}`}>{`${difference.hours
-                  .toString()
-                  .padStart(2, "0")}:${difference.minutes
-                  .toString()
-                  .padStart(2, "0")}:${difference.seconds
-                  .toString()
-                  .padStart(2, "0")}`}</div>
-              </div>
+          <div className="d-flex flex-column align-items-center justify-content-top">
+            <div className="">
+              <div className={`${styles.timer}`}>{time}</div>
+            </div>
+            <div className="">
+              <a
+                className="btn btn-outline-light py-3 px-5 fs-4 fw-bold d-flex gap-3 align-items-center justify-content-center"
+                href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+                  "https://tezosteusday.com"
+                )}&text=${encodeURIComponent(
+                  `I'm ready for #TezosTeusday\n${time} to Tezos Teusday!`
+                )}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BsTwitter size={48} /> Share
+              </a>
             </div>
           </div>
         </div>
@@ -84,7 +98,9 @@ const Home = () => {
       <footer className="bg-primary text-white border-top border-secondary">
         <div className="container-fluid px-5 py-3">
           <div className="d-flex align-items-center justify-content-end">
-            GitHub
+            <a className="link-light text-decoration-none" href="#">
+              GitHub
+            </a>
           </div>
         </div>
       </footer>
